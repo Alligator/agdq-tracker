3d676f719319e39eb0b04f88440a959c0d1b51b8
rrs <enemy.forest.brigade@gmail.com>
2020-01-04 00:05:19 +0000

    fix the tracker link not working

--- setup-new-marathon.sh
+++ setup-new-marathon.sh
@@ -10,8 +10,8 @@ echo "setting stuff up for $marathon_nice_name"
 
 echo "replacing variables in cron-job/get-data.py"
 sed -i \
-  -e "s/MARATHON_NAME/$marathon_name/g" \
   -e "s/MARATHON_NAME_FULL/$marathon_name_full/g" \
+  -e "s/MARATHON_NAME/$marathon_name/g" \
   -e "s/START_DATE/$marathon_start_date/g" \
   -e "s/END_DATE/$marathon_end_date/g" \
   -e "s/TWITCH_KEY/$twitch_key/g" \
@@ -19,6 +19,7 @@ sed -i \
 
 echo "replacing variables in web/index.js"
 sed -i \
+  -e "s/MARATHON_NAME_FULL/$marathon_name_full/g" \
   -e "s/MARATHON_NAME/$marathon_name/g" \
   -e "s/MARATHON_SHORT_NAME/$marathon_short_name/g" \
   -e "s/MARATHON_NICE_NAME/$marathon_nice_name/g" \
