435d690ef2a12c115c00247849bcaa552eb33d8f
rrs <enemy.forest.brigade@gmail.com>
2024-08-17 20:08:26 +0000

    change get-data to take cmd line args

--- cron-job/get-data.py
+++ cron-job/get-data.py
@@ -4,6 +4,7 @@ import sys
 import requests
 import re
 import configparser
+import argparse
 
 import lxml.html
 from datetime import datetime, timedelta
@@ -11,29 +12,56 @@ from dateutil import parser
 
 from pprint import pprint
 
-filename = '../web/MARATHON_NAME.json'
+arg_parser = argparse.ArgumentParser()
 
-# marathon start and end times go here, so I don't have to worry about turning
-# the cron job on or off at the right time
+arg_parser.add_argument('--name', required=True)
+arg_parser.add_argument('--type', required=True, choices=['gdq', 'gdqx', 'ff'])
+arg_parser.add_argument(
+    '--start-date',
+    required=True,
+    type=parser.parse)
+arg_parser.add_argument(
+    '--end-date',
+    required=True,
+    type=parser.parse)
+arg_parser.add_argument(
+    '--force',
+    action='store_true')
+arg_parser.add_argument('--twitch-client-id', required=True)
+arg_parser.add_argument('--twitch-access-token', required=True)
+arg_parser.add_argument('--tracker-marathon-name', required=True)
+arg_parser.add_argument('--tracker-marathon-id', required=True)
+arg_parser.add_argument('output_file')
+
+args = arg_parser.parse_args()
+
 current_time = time.time()
-start_date = parser.parse('START_DATE')
-end_date = parser.parse('END_DATE')
-if 'force' not in sys.argv and (datetime.utcnow() < start_date or datetime.utcnow() > end_date):
+start_date = args.start_date
+end_date = args.end_date
+now = datetime.utcnow().timestamp()
+if not args.force and (now < start_date.timestamp() or now > end_date.timestamp()):
   sys.exit(0)
 
 try:
-  current_stats = json.load(open(filename))
+  current_stats = json.load(open(args.output_file))
 except IOError as e:
-  current_stats = { 'viewers': [], 'games': [] }
+  current_stats = {
+    'marathon_name': args.name,
+    'marathon_type': args.type,
+    'viewers': [],
+    'games': [],
+  }
 
 # get viewers
 for i in range(3):
   try:
     headers = {
-      'Client-ID': 'TWITCH_CLIENT_ID',
-      'Authorization': 'Bearer TWITCH_ACCESS_TOKEN',
+      'Client-ID': args.twitch_client_id,
+      'Authorization': args.twitch_access_token,
     }
-    j = json.loads(requests.get('https://api.twitch.tv/helix/streams?user_id=22510310', headers=headers, verify=True, timeout=10.0).text)
+    resp = requests.get('https://api.twitch.tv/helix/streams?user_id=22510310', headers=headers, verify=True, timeout=10.0)
+    resp.raise_for_status()
+    j = resp.json()
     if len(j['data']) == 0:
         t = None
         break;
@@ -41,22 +69,22 @@ for i in range(3):
     break
   except Exception as e:
     # write out a null
-    sys.stderr.write('viewers ' + str(e) + '\n')
+    sys.stderr.write('viewers ' + repr(e) + '\n')
     t = None
 
 # get donations
 try:
-  j = json.loads(requests.get('https://gamesdonequick.com/tracker/event/MARATHON_NAME_FULL?json').text)
+  j = json.loads(requests.get(f'https://gamesdonequick.com/tracker/event/{args.tracker_marathon_name}?json').text)
   dn = float(j['agg']['amount'])
 except Exception as e:
-  sys.stderr.write('donations' + str(e) + '\n')
+  sys.stderr.write('donations' + repr(e) + '\n')
   dn = None
 
 current_stats['viewers'].append((current_time, t, dn))
 
 # get schedule
 try:
-  resp = requests.get('https://gamesdonequick.com/tracker/api/v2/events/MARATHON_ID/runs/')
+  resp = requests.get(f'https://gamesdonequick.com/tracker/api/v2/events/{args.tracker_marathon_id}/runs/')
   resp.raise_for_status()
   j = json.loads(resp.text)
 
@@ -75,7 +103,7 @@ try:
   current_stats['games'] = games
 except Exception as e:
   # on error don't change the games
-  sys.stderr.write('games ' + str(e) + '\n')
+  sys.stderr.write('games ' + repr(e) + '\n')
   pass
 
-json.dump(current_stats, open(filename, 'w'))
+json.dump(current_stats, open(args.output_file, 'w'))
